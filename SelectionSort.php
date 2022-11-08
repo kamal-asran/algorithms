5382b6b6414cd8595bc107082496f73757aada35
kamal-asran <prog.kamal.asran@gmail.com>
2022-11-08 14:14:59 +0200

    slection sort algorithm

---
+++ SelectionSort.php
@@ -0,0 +1,34 @@
+<?php
+//implementation of selection sort
+function selection_sort(&$array, $length){
+
+    for($i=0;$i<$length;$i++)
+    {
+        $min=$i;
+        for($j=$i+1;$j<$length;$j++)
+        {
+            //get the smallest number from the rest of array
+            if($array[$j] < $array[$min])
+            {
+                $min=$j;
+
+            }
+        }
+        //swap array[i] with array[min] 
+        if ($array[$i] > $array[$min])
+		{
+			$tmp = $array[$i];
+			$array[$i] = $array[$min];
+			$array[$min] = $tmp;
+		}
+    }
+
+}
+
+$array=[5,4,6,1,2,0];
+$length=count($array);
+selection_sort($array, $length);
+for ($i = 0; $i < $length; $i++)
+{
+    echo $array[$i] . " ";
+}
